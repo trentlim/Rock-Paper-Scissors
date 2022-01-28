d610db3fd632a4b22e318cae3ea3f2a67d1dc5a5
Trent Lim <trent.lim@datacom.co.nz>
2022-01-28 15:13:06 +1300

    Cleanup query selectors and remove message on new game

--- dist/main.js
+++ dist/main.js
@@ -3,55 +3,58 @@ const buttons = document.querySelectorAll('.btn-choice');
 let playerScore = 0;
 let computerScore = 0;
 
+const overlay = document.querySelector('#modal-overlay');
+const modalIcon = overlay.querySelector('img');
+const modalMessage = overlay.querySelector('h2');
+const modalButton = overlay.querySelector('button');
+const roundMessage = document.querySelector('#round-message');
+const winnerMessage = roundMessage.querySelector('h2');
+const roundResult = roundMessage.querySelector('h3');
+
 function computerPlay() {
     return choices[Math.floor(Math.random() * 3)];
 }
 
 function endGame(winner) {
-    const overlay = document.querySelector('#modal-overlay');
-    let icon = overlay.querySelector('img');
-    let message = overlay.querySelector('h2');
-    const button = overlay.querySelector('button');
-
-
     if (winner == 'player') {
-        icon.src = '../img/trophy.png';
-        message.innerHTML = 'You won the game!';
+        modalIcon.src = '../img/trophy.png';
+        modalMessage.innerHTML = 'You won the game!';
     } else if (winner == 'computer') {
-        icon.src = '../img/sad-face.png';
-        message.innerHTML = 'You lost the game!';
+        modalIcon.src = '../img/sad-face.png';
+        modalMessage.innerHTML = 'You lost the game!';
     }
 
     overlay.classList.toggle('hidden');
     overlay.classList.toggle('flex');
 
-    button.addEventListener('click', () => {
+    modalButton.addEventListener('click', () => {
         overlay.classList.toggle('hidden');
         overlay.classList.toggle('flex');
         playerScore = 0;
         computerScore = 0;
         document.querySelector('#player-score').innerHTML = playerScore;
         document.querySelector('#computer-score').innerHTML = computerScore;
+        roundMessage.classList.add('invisible');
     })
 }
 
 function displayMessage(winner, playerSelection, computerSelection) {
-    const message = document.querySelector('#message');
-    let winnerMessage = message.querySelector('h2');
-    let result = message.querySelector('h3');
+    // const message = document.querySelector('#message');
+    // let winnerMessage = message.querySelector('h2');
+    // let result = message.querySelector('h3');
 
     if (winner == 'player') {
         winnerMessage.innerHTML = 'You win!';
-        result.innerHTML = `You chose ${playerSelection} and computer chose ${computerSelection}`;
+        roundResult.innerHTML = `You chose ${playerSelection} and computer chose ${computerSelection}`;
     } else if (winner == 'computer') {
         winnerMessage.innerHTML = 'Computer wins!';
-        result.innerHTML = `You chose ${playerSelection} and computer chose ${computerSelection}`;
+        roundResult.innerHTML = `You chose ${playerSelection} and computer chose ${computerSelection}`;
     } else {
         winnerMessage.innerHTML = 'It\'s a tie!';
-        result.innerHTML = `You both chose ${playerSelection}`;
+        roundResult.innerHTML = `You both chose ${playerSelection}`;
     }
 
-    message.classList.remove('invisible');
+    roundMessage.classList.remove('invisible');
 }
 
 
